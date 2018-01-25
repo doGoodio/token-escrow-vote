57269a098314227cd2e450048da6dd7d2145e6f6
Nick Geoca <nickgeoca@gmail.com>
2018-01-25 15:46:17 -0600

    formal alloc vote test

--- test/Escrow.js
+++ test/Escrow.js
@@ -20,26 +20,19 @@ contract('Escrow', function (accounts) {
   const payoutAddress = accounts[4];
   const arbitrator = accounts[5];
 
-//  var escrow;
-  var controller = account1;
-  var token;
-
-  describe('test 1', async () => {
+  describe('escrow init tests', async () => {
 
     beforeEach(async () => {
-
-      const tokenSupply = new BigNumber(1000);
+      const tokenSupply = new BigNumber(1000000000);
       const tokenName = 'Test token'
       const tokenSymbol = 'test'
       const tokenDecimals = new BigNumber(18);
 
       token = await ERC20.new(tokenSupply, tokenName, tokenDecimals, tokenSymbol, {from: account1});
       await EscrowInterface.init({from: account1});
-
-      // var numRounds  = new BigNumber(3);
-      // var tx = await escrowFactory.createEscrow(numRounds, controller, minimetoken.address, {from: account1});
     });
 
+    /*
     it('1', async () => {
       const numRounds = new BigNumber(3);
       const minVotes = new BigNumber(30);
@@ -47,13 +40,14 @@ contract('Escrow', function (accounts) {
       const allocEndTime = new BigNumber(100);
       var id;
       function setId(_id) { id = _id; }
+      const testBal = new BigNumber(84729832);
 
       await EscrowInterface.createEscrow(setId, numRounds, arbitrator, token.address, payoutAddress, minVotes, allocStartTime, allocEndTime, {from: account1});
       // need blocking, since rely on id
       await EscrowInterface.setBlockTime(new BigNumber(50), {from: account1});
 
       // need to consider decimals for voting. e.g. 10**18 tokens -> 10**9 votesAllocated.. how many vote decimals are there?
-      await token.transfer(account2, 10 * 10, {from: account1});
+      await token.transfer(account2, testBal, {from: account1});
       await token.transfer(account3, 8, {from: account1});
       await token.transfer(account4, 12, {from: account1});
 
@@ -65,10 +59,72 @@ contract('Escrow', function (accounts) {
       const bal = await token.balanceOf(account2);
       
       // log data
-      console.log('acutal vote weight = ' + vw);
-      console.log('expected voted weight = 10');
-      console.log('token bal = ' + bal);
+      assert.equal(ownerBalance.toNumber(), aixInExchanger.toNumber());
+
+      console.log('acutal vote weight = ' + vw.toString(10));
+      console.log('expected voted weight = ' + testBal.sqrt().floor().toString(10));
+      console.log('token bal = ' + bal.toString(10));
       for (var i in data) console.log('Escrow data key ' + i + ' -> ' + data[i]);
+    });
+    */
+  });
+
+  describe('escrow scenario tests', async () => {
+    const numRounds = new BigNumber(3);
+    const minVotes = new BigNumber(30);
+    const allocStartTime = new BigNumber(10);
+    const allocEndTime = new BigNumber(100);
+    var id;
+    var token;
+    
+    beforeEach(async () => {
+      function setId(_id) { id = _id; }
+      const tokenSupply = new BigNumber(1000000000);
+      const tokenName = 'Test token'
+      const tokenSymbol = 'test'
+      const tokenDecimals = new BigNumber(18);
+
+      token = await ERC20.new(tokenSupply, tokenName, tokenDecimals, tokenSymbol, {from: account1});
+      await EscrowInterface.init({from: account1});
+      await EscrowInterface.createEscrow(setId, numRounds, arbitrator, token.address, payoutAddress, minVotes, allocStartTime, allocEndTime, {from: account1});
+    });
+
+
+    // need to consider decimals for voting. e.g. 10**18 tokens -> 10**9 votesAllocated.. how many vote decimals are there?
+    it('allocates voting', async () => {
+      // vars
+      const bal2 = new BigNumber(84729832);
+      const bal3 = new BigNumber(238913);
+      const bal4 = new BigNumber(98284982);
+      var b2,b3,b4;
+
+      // set block time
+      await EscrowInterface.setBlockTime(new BigNumber(50), {from: account1});
+      
+      // Alloc token balances and double check
+      await token.transfer(account2, bal2, {from: account1});
+      await token.transfer(account3, bal3, {from: account1});
+      await token.transfer(account4, bal4, {from: account1});
+      b2 = await token.balanceOf(account2);
+      b3 = await token.balanceOf(account3);
+      b4 = await token.balanceOf(account3);
+      assert.equal(bal2.toNumber(), b2.toNumber());
+      assert.equal(bal3.toNumber(), b3.toNumber());
+      assert.equal(bal3.toNumber(), b4.toNumber());
+
+
+      // alloc votes and get vote weight
+      await EscrowInterface.allocVotes(id, {from: account2});
+      await EscrowInterface.allocVotes(id, {from: account3});
+      await EscrowInterface.allocVotes(id, {from: account4});
+      const vw2 = await EscrowInterface.get_voteWeight(id, account2);
+      const vw3 = await EscrowInterface.get_voteWeight(id, account3);
+      const vw4 = await EscrowInterface.get_voteWeight(id, account4);
+      
+      // tests
+      assert.equal(vw2.toNumber(), bal2.sqrt().floor().toNumber());
+      assert.equal(vw3.toNumber(), bal3.sqrt().floor().toNumber());
+      assert.equal(vw4.toNumber(), bal4.sqrt().floor().toNumber());
     });
   });
 });
